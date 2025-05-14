c719c61082b077504e3e96e79803031e5d49731f
HuacongXie <solarsys@outlook.com>
2025-05-14 15:15:48 -0400

    added account path on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import Mine from './components/Mine';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Account from "./containers/Account/Account";
+import Login from "./containers/Account/Login";
+import Register from "./containers/Account/Register";
+
+const router = createBrowserRouter([
+
+    {
+        path: "/account",
+        element: <Account />,
+        children: [{
+            path: "/account/login",
+            element: <Login />
+        }, {
+            path: "/account/register",
+            element: <Register />
+        }
+        ]
+    },
+])
 
 function App() {
-    return (
-             <Mine/>
-    )
+    return <RouterProvider router={router}/>;
 }
 
 export default App;
