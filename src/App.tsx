243b8bf79e08ee81562526f969e3fc36039dea99
HuacongXie <solarsys@outlook.com>
2025-05-14 21:50:44 -0400

    added Guide and Home components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import Mine from './components/Mine';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+
+import 'normalize.css';
+import './styles/border.css';
+import './styles/base.css';
+import {createHashRouter, RouterProvider} from 'react-router-dom';
+import React from "react";
 import Account from "./containers/Account/Account";
 import Login from "./containers/Account/Login";
 import Register from "./containers/Account/Register";
+import Guide from "./containers/Guide/Guide";
+import Home from "./containers/Home/Home";
 
-const router = createBrowserRouter([
+const router = createHashRouter([{
+        path: "/",
+        element: <Guide />
+    },
 
     {
         path: "/account",
         element: <Account />,
         children: [{
-            path: "/account/login",
-            element: <Login />
-        }, {
-            path: "/account/register",
-            element: <Register />
-        }
+                path: "/account/login",
+                element: <Login />
+            }, {
+                path: "/account/register",
+                element: <Register />
+            }
         ]
+    },
+    {
+        path: "/home",
+        element: <Home />
     },
 ])
 
